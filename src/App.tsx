34cff309268ea98f3c8e108723f4b871ddf017fd
Marta García Ferreiro <martagarciaferreiro@gmail.com>
2025-08-08 15:54:38 -0400

    Add src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import TodoList from './components/TodoList';
+import AddTodoForm from './components/AddTodoForm';
+import './App.css';
+
+function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, text: 'Learn React', completed: false },
+    { id: 2, text: 'Build a TODO app', completed: false },
+  ]);
+
+  const addTodo = (text: string) => {
+    const newTodo = {
+      id: Date.now(),
+      text,
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+  };
+
+  const toggleTodo = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const editTodo = (id: number, newText: string) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, text: newText } : todo
+      )
+    );
+  };
+
+  return (
+    <div className="App">
+      <h1>TODO App</h1>
+      <AddTodoForm addTodo={addTodo} />
+      <TodoList
+        todos={todos}
+        toggleTodo={toggleTodo}
+        deleteTodo={deleteTodo}
+        editTodo={editTodo}
+      />
+    </div>
+  );
+}
+
+export default App;
